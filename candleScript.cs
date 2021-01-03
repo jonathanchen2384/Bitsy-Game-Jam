a466a8176341baf1890d698dca1a20f479cd6443
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-03 06:24:20 -0800

    Create candleScript.cs

---
+++ candleScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class candleScript : MonoBehaviour
+{
+    public GameObject lightUp;
+    public GameObject closedDoor;
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")||collision.CompareTag("Flame")) {
+
+            closedDoor.GetComponent<doorScript>().emptyLightsLeft -= 1;
+            Instantiate(lightUp, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
