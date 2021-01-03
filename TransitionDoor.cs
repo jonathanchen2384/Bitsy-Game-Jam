0b5c71ede940629ac0917a26f283a1ff665c4bd6
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-03 06:32:50 -0800

    Create TransitionDoor.cs

---
+++ TransitionDoor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionDoor : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+        transform.Rotate(0f, 0f, 5f, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //transition to next Scene
+        }
+    }
+}
