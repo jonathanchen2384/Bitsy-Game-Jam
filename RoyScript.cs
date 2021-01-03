81e9827e35d4358ab4189f0dfb70b98b9d09a022
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-03 06:30:13 -0800

    Create RoyScript.cs

---
+++ RoyScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoyScript : MonoBehaviour
+{
+    public int RoyHealth;
+    public float fallSpeed;
+    public float timeDeath;
+
+    public float attackTime;
+    private float duration;
+    public float SinkPos;
+
+    public Animator anim;
+
+    void Start()
+    {
+        RoyHealth = 3;
+        duration = attackTime;
+    }
+
+    void FixedUpdate()
+    {
+        if (RoyHealth <= 0)
+        {
+            // Idle Mode
+            // Instantiate rumbling death effect
+            anim.SetBool("isAttacking", false);
+
+            transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+
+            if (transform.position.y <= SinkPos)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        else
+        {
+            attack();
+        }
+    }
+
+    public void attack()
+    {
+        if(duration <= 0)
+        {
+            //attack Mode
+            anim.SetBool("isAttacking", true);
+            duration = attackTime;
+        }
+
+        else
+        {
+            duration -= Time.deltaTime;
+        }
+    }
+}
