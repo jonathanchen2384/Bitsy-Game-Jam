4bb0429b8bd672c9a40c4ad69df1eee23c9e4272
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-03 06:28:23 -0800

    Create managementScript.cs

---
+++ managementScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class managementScript : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            SceneManager.LoadScene("A Menu");
+        }
+    }
+}
